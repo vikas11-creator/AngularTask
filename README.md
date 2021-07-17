9b301cc2d60488c61b4e9a80c90faa0e91a1a5a4
Vikas Sharma <88000906@corp.tmf.co.in>
2021-07-18 01:16:28 +0530

    first commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-#Angular Task
+# Angular Task
 
-##Output
+## Output
 ![alt text](./ss/ss1.png "")
 ![alt text](./ss/ss2.png "")
 ![alt text](./ss/ss3.png "")
--- src/app/end.spec.ts
+++
@@ -1,7 +0,0 @@
-import { End } from './end';
-
-describe('End', () => {
-  it('should create an instance', () => {
-    expect(new End()).toBeTruthy();
-  });
-});
